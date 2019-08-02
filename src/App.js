761227e2fc78a4fe4d03fe25e64d98ccd1734ef3
Cristof Morissette <cristofmorissette@Cristofs-MacBook-Air.local>
2019-08-02 16:00:53 -0600

    working on redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 
-function App() {
+class App extends Component {
+  
+  render(){
   return (
-    <div className="App">
+    <div>
      
     </div>
   );
+}
 }
 
 export default App;
