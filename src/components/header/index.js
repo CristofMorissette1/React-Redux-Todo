5adee3c59a50a604ee86f7e28d3d9b9cd1c3bdd0
Cristof Morissette <cristofmorissette@Cristofs-MacBook-Air.local>
2019-08-02 18:59:24 -0600

    boilerplater

---
+++ src/components/header/index.js
@@ -0,0 +1,13 @@
+import react, { component } from 'react'
+
+class Header extends Component{
+    
+
+    render(){
+        return(
+            <h1>Todo Application</h1>
+        )
+    }
+}
+
+export default Header
---
+++ src/components/taskbar/index.js
@@ -0,0 +1,16 @@
+import react, { component } from 'react'
+
+class TaskBar extends Component{
+
+
+    render(){
+        return(
+            <div>
+                <input type="text" ref="task" placeholder="Add tasks here"/>
+                <button>Add Task</button>
+            </div>
+        )
+    }
+}
+
+export default TaskBar
